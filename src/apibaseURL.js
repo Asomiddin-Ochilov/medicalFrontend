73d4dd95b77fbc45303158a2070686d0e0639e66
Asomiddin Ochilov <84656537+Asomiddin-Ochilov@users.noreply.github.com>
2026-04-20 12:12:51 +0500

    new

--- src/apibaseURL.js
+++ src/apibaseURL.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const instance = axios.create({
-  baseURL: "https://apimedical.site/api",
+  baseURL: "https://www.apimedical.site/api",
    headers: {
     "Content-Type": "application/json"
   }
